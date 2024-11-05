89914cef4fe72fef975ac0a5fba12a37b85bbcd2
Nelson Martell <nelson6e65@gmail.com>
2024-11-05 11:10:43 -0500

    feat(data): add faker generator for `ILotteryDraw`
    Install `@faker-js/faker`.

--- src/models/api/lottery-draw.ts
+++ src/models/api/lottery-draw.ts
@@ -1,8 +1,29 @@
+import { faker, simpleFaker } from '@faker-js/faker';
+
+/* eslint-disable @typescript-eslint/no-namespace */
+
 export interface ILotteryDraw {
   id: string;
   name: string;
   logo: string;
   jackpot: IPrice;
+}
+
+export namespace ILotteryDraw {
+  export const generateFakeData = (): ILotteryDraw => {
+    const id = simpleFaker.string.uuid();
+
+    return {
+      id,
+      // logo: `https://picsum.photos/seed/${id}/${faker.number.int({ min: 64, max: 256, multipleOf: 16 })}/${faker.number.int({ min: 64, max: 256, multipleOf: 16 })}`,
+      logo: `https://picsum.photos/seed/${id}/256/256`,
+      name: 'Probatio ' + faker.lorem.word({ length: { min: 6, max: 8 } }),
+      jackpot: {
+        amount: faker.number.int({ min: 1000, max: 999999999 }),
+        currency: 'PEN',
+      },
+    };
+  };
 }
 
 export interface ILotteryDrawWithDetails extends ILotteryDraw {
