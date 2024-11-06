a8da714702565e35eee2092dd2f69a68b3670f84
Nelson Martell <nelson6e65@gmail.com>
2024-11-06 08:20:49 -0500

    fix: typo

--- src/app/lotteries/[id]/not-found.tsx
+++ src/app/lotteries/[id]/not-found.tsx
@@ -5,7 +5,7 @@ export default function LotteryDrawNotFound() {
     <div className="flex h-full flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-red-500">404</h1>
 
-      <p className="text-2xl">Lottery draw does not exists!</p>
+      <p className="text-2xl">Lottery draw does not exist!</p>
 
       <span>
         <AppLink href="/lotteries">Go back to the list</AppLink>
